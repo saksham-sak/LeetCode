12ffc6ebe8c8a6a16a88d5899d51476ca471d1e1
saksham patel <sakshamp670@gmail.com>
2026-08-28 23:16:00 +0530

    Time: 2549 ms (5.05%), Space: 13.3 MB (36.83%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        length = len(nums)
+
+        for i in range(length + 1):
+            if i not in nums:
+                return i
+
+        
