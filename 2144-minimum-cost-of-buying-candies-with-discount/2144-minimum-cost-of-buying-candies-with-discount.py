d6ecef99d3db48e44b21188b68da89519e4e1e95
saksham patel <sakshamp670@gmail.com>
2026-09-13 00:15:36 +0530

    Time: 1 ms (51.65%), Space: 12.3 MB (54.13%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,12 +1,9 @@
 class Solution:
     def minimumCost(self, cost):
         sumOfCost = sum(cost)
-        if (len(cost) < 3):
-            return sumOfCost
-        else:
-            cost.sort(reverse=True)
-            for i in range(2,len(cost),3):
-                sumOfCost -= cost[i]
-            return sumOfCost
+        cost.sort(reverse=True)
+        for i in range(2,len(cost),3):
+            sumOfCost -= cost[i]
+        return sumOfCost
           
              
