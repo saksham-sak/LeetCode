e4f3258987427e94bde4748d6d0b43dd174a2806
saksham patel <sakshamp670@gmail.com>
2026-09-15 20:52:30 +0530

    Time: 0 ms (100%), Space: 12.9 MB (66.48%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
+            
