6b6c4febc8399e9f615fe1c0727f38d38d4397e8
saksham patel <sakshamp670@gmail.com>
2026-09-24 16:11:00 +0530

    Time: 15 ms (19.56%), Space: 12.3 MB (61.59%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def smallestIndex(self, nums):
-        minAll = 10000
+        
 
         for i in range(len(nums)):
             word = str(nums[i])
             sumEl = 0
             if(len(word) == 1 and nums[i] ==i):
-                minAll = min(minAll,i)
+                return i
             else:
                 for ch in word:
                     sumEl += int(ch)
 
                 if(sumEl == i):
-                    minAll = min(minAll,i)
+                    return i
 
-        if(minAll != 10000):
-            return minAll
         return -1
         
 
