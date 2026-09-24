7cc3b5a89b37cfdf7ed02a9169a60d8c71c78926
saksham patel <sakshamp670@gmail.com>
2026-09-24 16:13:43 +0530

    Time: 3 ms (75.36%), Space: 12.3 MB (92.03%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -5,14 +5,11 @@ class Solution(object):
         for i in range(len(nums)):
             word = str(nums[i])
             sumEl = 0
-            if(len(word) == 1 and nums[i] ==i):
-                return i
-            else:
-                for ch in word:
-                    sumEl += int(ch)
+            for ch in word:
+                sumEl += int(ch)
 
-                if(sumEl == i):
-                    return i
+            if(sumEl == i):
+                return i
 
         return -1
         
